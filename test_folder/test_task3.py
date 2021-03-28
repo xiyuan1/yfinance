b7e63ebd8e0f5cbc2295262cef44b80f513f076b
Khang <56883598+khanguyen7@users.noreply.github.com>
2021-03-27 18:24:30 -0600

    Create test_task3.py

---
+++ test_folder/test_task3.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+import unittest
+from unittest.mock import Mock
+from unittest.mock import patch
+import pandas as pd
+
+symbols = ['MSFT', 'IWO', 'VFINX', '^GSPC', 'BTC-USD']
+tickers = [yf.Ticker(symbol) for symbol in symbols]
+
+
+class TestEvent1(unittest.TestCase):
+    def __init__(self):
+        self.msft = yf.Ticker("MSFT")
+        self.utils_mock = patch('yfinance.base.utils.get_json', autospec=True).start()
+        self.addCleanup(patch.stopall)
+
+    def test_proper_date(self):
+        data = {'earningsDate': [1619481600, 1620000000], 'earningsAverage': 1.77, 'earningsLow': 1.69,
+                'earningsHigh': 1.93, 'revenueAverage': 41030500000, 'revenueLow': 40541000000,
+                'revenueHigh': 41894000000}
+        
+        calendar = self.msft.fill_calendar(data)
+        self.assertNotEqual(calendar['earningsDate'], [1619481600, 1620000000])
+
+if __name__ == '__main__':
+    unittest.main()
